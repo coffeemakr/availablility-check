f0f38943e4f92032092e5b9c31696f12c9441b9e
coffeemakr <git@unstable.ch>
2020-05-10 11:59:47 +0200

    Rename to lauft

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 
 from setuptools import setup, find_packages
 
-setup(name='avail',
+setup(name='lauft',
       version='1.0',
       description='Availabilty Check',
       author_email='git@unstable.ch',
       install_requires=["requests"],
-      py_modules=['avail'],
+      py_modules=['lauft'],
       entry_points={
           "console_scripts": [
-              "avail = avail:run"
+              "lauft = lauft:run"
           ]
       }
     )
